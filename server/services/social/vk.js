217dbbf4f2c5ac03fb77e3722b36e051facb9ec8
olegdev <s2ferr@gmail.com>
2016-03-11 11:55:02 +0300

    откатил отладку

--- server/services/social/vk.js
+++ server/services/social/vk.js
@@ -33,14 +33,16 @@ Service.prototype.auth = function(request, callback) {
 				if (user) {
 					callback(null, user.get('_id'));
 				} else {
-
-					userService.register({
+					console.log('request');
+					me.vkApi.request('users.get', {user_id: request.viewer_id, fields: ['photo_50'], lang: 'ru'}, function(resp) {
+						if (resp && resp.response && resp.response.length) {
+							userService.register({
 						   		auth: {
 						   			vkId: request.viewer_id
 						   		},
 						   		info: {
-						   			title: '343',
-						   			img: '',
+						   			title: resp.response[0].first_name + ' ' + resp.response[0].last_name,
+						   			img: resp.response[0].photo_50,
 						   		}
 						   	}, function(err, userModel) {
 								if (!err) {
@@ -49,28 +51,10 @@ Service.prototype.auth = function(request, callback) {
 									callback(error.factory('vk', 'auth', 'User register error ' + err, logger));
 								}
 							});
-
-					// me.vkApi.request('users.get', {user_id: request.viewer_id, fields: ['photo_50'], lang: 'ru'}, function(resp) {
-					// 	if (resp && resp.response && resp.response.length) {
-					// 		userService.register({
-					// 	   		auth: {
-					// 	   			vkId: request.viewer_id
-					// 	   		},
-					// 	   		info: {
-					// 	   			title: resp.response[0].first_name + ' ' + resp.response[0].last_name,
-					// 	   			img: resp.response[0].photo_50,
-					// 	   		}
-					// 	   	}, function(err, userModel) {
-					// 			if (!err) {
-					// 				callback(null, userModel.model.id);
-					// 			} else {
-					// 				callback(error.factory('vk', 'auth', 'User register error ' + err, logger));
-					// 			}
-					// 		});
-					// 	} else {
-					// 		callback(error.factory('vk', 'auth', 'Data from vk api is invalid ' + JSON.stringify(resp), logger));					
-					// 	}
-					// });
+						} else {
+							callback(error.factory('vk', 'auth', 'Data from vk api is invalid ' + JSON.stringify(resp), logger));					
+						}
+					});
 				}
 			}
 		});
@@ -133,7 +117,6 @@ Service.prototype.order = function(data, callback) {
 }
 
 Service.prototype.checkAuthKey = function(request) {
-	return true;
 	var str = request.api_id + '_' + request.viewer_id + '_' + config.app_secret;
 	return request.auth_key === md5(str);
 }
